b2d3fd68c9059a1bd9e077859c672f4488a04142
Jon Tore Hafstad <jontore@gmail.com>
2019-05-30 16:42:59 +0200

    Updates

---
+++ TODO.md
@@ -0,0 +1,11 @@
+TODO
+Web-aria
+
+What did I learn
+Use basic HTML elements, navigation
+Tables are better for table content
+Aria roles,
+Error messages should be grouped easier to get to
+Keep focus and tabulation
+No hidden button
+Good product descriptions
---
+++ precache-manifest.9c59c4ef92e0ea21a0faf3702482f349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacb98506ac9c4280001f3f1a4754af8",
+    "url": "/accessibility-patterns/index.html"
+  },
+  {
+    "revision": "5ddf9fdffb653c96839e",
+    "url": "/accessibility-patterns/static/css/main.e918bcd7.chunk.css"
+  },
+  {
+    "revision": "37f1c1b2cff53329a640",
+    "url": "/accessibility-patterns/static/js/2.c7a8a559.chunk.js"
+  },
+  {
+    "revision": "5ddf9fdffb653c96839e",
+    "url": "/accessibility-patterns/static/js/main.38922432.chunk.js"
+  },
+  {
+    "revision": "9ffd22014906a0a01dca",
+    "url": "/accessibility-patterns/static/js/runtime~main.c07a0e1f.js"
+  }
+]);
